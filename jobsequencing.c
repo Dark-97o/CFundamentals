447e2af1e6809dce56db67c09fbc712855e1c14e
Subh97-0 <114503763+Dark-97o@users.noreply.github.com>
2025-02-20 16:16:42 +0530

    commit message

--- jobsequencing.c
+++ jobsequencing.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Structure to represent a job
 typedef struct {
     char* name;
     int deadline;
     int profit;
 } Job;
 
-// Function to sort jobs by profit in descending order
 void sortJobs(Job jobs[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = i+1; j < n; j++) {
@@ -22,30 +20,20 @@ void sortJobs(Job jobs[], int n) {
     }
 }
 
-// Function to perform job sequencing
 void job_sequencing(Job jobs[], int n) {
-    // Sort jobs by profit
     sortJobs(jobs, n);
-
-    // Find maximum deadline to create time slots
     int max_deadline = 0;
     for (int i = 0; i < n; i++) {
         if (jobs[i].deadline > max_deadline)
             max_deadline = jobs[i].deadline;
     }
 
-    // Array to track free time slots
     bool time_slots[max_deadline];
     for (int i = 0; i < max_deadline; i++)
         time_slots[i] = false;
-
-    // Array to store selected jobs
     char* result[max_deadline];
     int total_profit = 0;
-
-    // Assign jobs to time slots
     for (int i = 0; i < n; i++) {
-        // Find a free slot for this job
         for (int j = jobs[i].deadline - 1; j >= 0; j--) {
             if (!time_slots[j]) {
                 result[j] = jobs[i].name;
@@ -56,7 +44,6 @@ void job_sequencing(Job jobs[], int n) {
         }
     }
 
-    // Print the result
     printf("Selected Jobs:\n");
     for (int i = 0; i < max_deadline; i++) {
         if (time_slots[i])
@@ -66,7 +53,6 @@ void job_sequencing(Job jobs[], int n) {
 }
 
 int main() {
-    // Initialize jobs
     Job jobs[] = {
         {"Maruti Wagon R", 5, 500},
         {"Nisan Micra", 3, 400},
@@ -76,9 +62,6 @@ int main() {
         {"Maruti Grand Viattara", 2, 1100}
     };
     int n = sizeof(jobs)/sizeof(jobs[0]);
-
-    // Perform job sequencing
     job_sequencing(jobs, n);
-
     return 0;
 }
--- testjonseq.c
+++ testjonseq.c
@@ -1,58 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
-// A structure to represent a job
 typedef struct {
-    char id;     // Job ID
-    int deadline; // Deadline of job
-    int profit;  // Profit if job is completed before or on deadline
+    char* name;
+    int deadline;
+    int profit;
 } Job;
 
-// Function to compare two jobs according to their profits
-int compare(const void *a, const void *b) {
-    Job *job1 = (Job *)a;
-    Job *job2 = (Job *)b;
-    return job2->profit - job1->profit;
+void sortJobs(Job jobs[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (jobs[i].profit < jobs[j].profit) {
+                Job temp = jobs[i];
+                jobs[i] = jobs[j];
+                jobs[j] = temp;
+            }
+        }
+    }
 }
 
-// Function to print the sorted deadlines with respect to profit and job index
 void job_sequencing(Job jobs[], int n) {
-    // Sort all jobs according to decreasing order of profit
-    qsort(jobs, n, sizeof(Job), compare);
-
-    int result[n]; // To store result (sequence of jobs)
-    int slot[n];   // To keep track of free time slots
-
-    // Initialize all slots to be free
-    for (int i = 0; i < n; i++)
-        slot[i] = 0;
-
-    // Iterate through all given jobs
+    sortJobs(jobs, n);
+    int dmax = 0;
     for (int i = 0; i < n; i++) {
-        // Find a free slot for this job (Note that we start from the last possible slot)
+        if (jobs[i].deadline > dmax)
+            dmax = jobs[i].deadline;
+    }
+    bool time_slots[dmax];
+    for (int i = 0; i < dmax; i++)
+        time_slots[i] = false;
+    char* result[dmax];
+    int total_profit = 0;
+    for (int i = 0; i < n; i++) {
         for (int j = jobs[i].deadline - 1; j >= 0; j--) {
-            // Free slot found
-            if (slot[j] == 0) {
-                result[j] = i;  // Add this job to result
-                slot[j] = 1;    // Mark this slot as occupied
+            if (!time_slots[j]) {
+                result[j] = jobs[i].name;
+                time_slots[j] = true;
+                total_profit += jobs[i].profit;
                 break;
             }
         }
     }
 
-    // Print the result
-    printf("Job sequence with maximum profit:\n");
-    for (int i = 0; i < n; i++) {
-        if (slot[i]) {
-            printf("Job ID: %d, Deadline: %d, Profit: %d\n", jobs[result[i]].id, jobs[result[i]].deadline, jobs[result[i]].profit);
-        }
+    printf("Sequenced Jobs:\n");
+    for (int i = 0; i < dmax; i++) {
+        if (time_slots[i])
+            printf("%d> %s\n",i+1, result[i]);
     }
+    printf("Total Profit: %d\n", total_profit);
 }
 
 int main() {
-    Job jobs[] = {{"Car", 2, 100}, {2, 1, 19}, {3, 2, 27}, {4, 1, 25}, {5, 3, 15}};
-    int n = sizeof(jobs) / sizeof(jobs[0]);
-
+    Job jobs[] = {
+        {"Customer 1 \t Maruti Wagon R", 5, 500},
+        {"Customer 2 \t Nisan Micra", 3, 400},
+        {"Customer 3 \t Toyota Innova", 3, 1200},
+        {"Customer 4 \t TATA Neon", 2, 700},
+        {"Customer 5 \t Renault Kiger", 4, 750},
+        {"Customer 6 \t Maruti Grand Vittara", 2, 1100}
+    };
+    int n = sizeof(jobs)/sizeof(jobs[0]);
     job_sequencing(jobs, n);
     return 0;
 }
